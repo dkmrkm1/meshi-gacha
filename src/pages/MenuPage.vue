d46d2842e85ba490a3df49da25875c0127fe7e3d
Duuun <daimura10@gmail.com>
2018-11-14 18:50:23 +0900

    Fixed #8. Updated contents in sidenav

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -4,22 +4,27 @@
     <div class="header">
       <img src="../assets/logo.png" alt="meshi-logo">
     </div>
-
+    <div class="sidenav__msg">
+      <div class="sidenav__msg__title">
+        <p>このアプリの使い所</p>
+      </div>
+      <p>今日の晩ご飯が決められない時や精神的に疲れている時に使えるよ！</p>
+    </div>
+    <div class="sidenav__msg">
+      <div class="sidenav__msg__title">
+        <p>使用方法</p>
+      </div>
+      <p>1. 自分がよく行くお店や食べる料理をリストアップしてね。</p>
+      <p>2. 「今日のメシを決める！」を押すとあなたの晩ご飯をランダムに決めてくれるよ♪</p>
+    </div>
+    <div class="sidenav__msg">
+      <div class="sidenav__msg__title">
+        <p>ちなみに...</p>
+      </div>
+      <p>スマホの方が使いやすいよ♪　気に入った人は「ホーム画面に追加」してね♪</p>
+    </div>
     <v-ons-list-title style="margin-right:10px; text-align:right; color:white">@duuun02</v-ons-list-title>
-    <!--
-    <v-ons-list>
-      <router-link to="/about">
-        <v-ons-list-item modifier="chevron">
-          <div>hogehoge</div>
-        </v-ons-list-item>
-      </router-link>
-
-      <v-ons-list-item modifier="chevron" v-for="item in essentialLinks" v-link="item.link" :key="item.link">
-        <div class="left"><v-ons-icon fixed-width :icon="item.icon"></v-ons-icon></div>
-        <div class="center">{{ item.label }}</div>
-      </v-ons-list-item>
-    </v-ons-list>
-    -->
+    
   </v-ons-page>
 </template>
 
@@ -59,16 +64,26 @@ export default {
   text-align: center;
   margin-top: 40px;
 }
-
 img {
   max-width: 150px;
 }
-
 ons-list-title {
   text-transform: none;
 }
-
 ons-list-item {
   cursor: pointer;
+}
+.sidenav__msg {
+  margin: 10px;
+  color: #fff;
+  font-family: "Kosugi maru", sans-serif;
+}
+.sidenav__msg__title {
+  font-weight: bold;
+  margin-left: 10px;
+  font-family: "Kosugi maru", sans-serif;
+}
+.sidenav__msg__list {
+  list-style-type: none;
 }
 </style>
